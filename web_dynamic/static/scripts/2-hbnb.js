f1023da62c26fbcef4f19c0ecc34a7d9147e34c2
Dev-Kings <kingasiadavid41@gmail.com>
2024-08-11 16:50:43 +0300

    implemented checking the API status and changing div element class

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,4 +14,20 @@ $(document).ready(function () {
     const amenitiesList = Object.values(selectedAmenities).join(', ');
     $('div.amenities h4').text(amenitiesList);
   });
+
+  const apiUrl = 'http://127.0.0.1:5001/api/v1/status/';
+	
+  function checkApiStatus() {
+    $.get(apiUrl, function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+	$('#api_status').removeClass('available');
+      }
+    }).fail(function () {
+	$('#api_status').removeClass('available');
+    });
+  }
+
+  checkApiStatus();
 });
